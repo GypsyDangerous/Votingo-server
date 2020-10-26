91a474245b808b0817f43783b4a3bdc1c6875742
David Snyder <davidgraygs4@gmail.com>
2020-10-26 09:41:34 -0400

    improved type checking and added colors

--- src/api/polls.ts
+++ src/api/polls.ts
@@ -2,32 +2,43 @@ import express from "express";
 const router = express.Router();
 import Joi from "joi";
 import uid from "uid";
-
+import randomColor from "randomcolor";
 import Poll from "../models/poll.model";
 
 const pollSubmitSchema = Joi.object({
 	name: Joi.string().min(3).max(25).required(),
 	description: Joi.string().min(3).max(500),
-	options: Joi.array().min(2).unique(),
+	options: Joi.array().min(2).unique().required(),
 	private: Joi.boolean(),
 });
+
+interface PollType {
+	options: {
+		[option: string]: { color: string; value: number };
+	};
+	uuid: string;
+	creator: "anonymous" | string;
+	private?: boolean;
+	name: string;
+	description?: string;
+}
 
 router.post("/create", async (req, res, next) => {
 	const { value, error } = pollSubmitSchema.validate(req.body);
 	if (error) {
-		res.status(400).json({ code: 400, message: error.details[0].message });
+		return res.status(400).json({ code: 400, message: `invalid poll data: ${error.details[0].message}` });
 	} else {
 		const id = uid();
-		const pollObj = {
+		const pollObj: PollType = {
 			...value,
-			options: value.options.reduce((acc: any, cur: string) => ({ ...acc, [cur]: 0 }), {}),
+			options: value.options.reduce((acc: any, cur: string) => ({ ...acc, [cur]: { value: 0, color: randomColor() } }), {}),
 			uuid: id,
 			creator: "anonymous",
 			private: !!value.private,
 		};
 		const newPoll = new Poll(pollObj);
 		await newPoll.save();
-		res.json({ code: 200, message: "poll created", id });
+		res.json({ code: 200, message: "poll created Successfully", id });
 	}
 });
 
@@ -48,6 +59,7 @@ router.patch("/vote/:id", async (req, res, next) => {
 });
 
 router.delete("/delete/:id", async (req, res, next) => {
+	// check permissions
 	const { id } = req.params;
 	await Poll.findOneAndDelete({ uuid: id });
 
@@ -58,18 +70,18 @@ router.get("/:id", async (req, res, next) => {
 	const { id } = req.params;
 	const poll = await Poll.findOne({ uuid: id });
 	if (!poll) {
-		res.status(400).json({ code: 400, message: "invalid message id" });
+		return res.status(400).json({ code: 400, message: "invalid message id" });
 	}
 	res.json(poll);
 });
 
 router.get("/", async (req, res, next) => {
-	try{
-		const polls = await Poll.find()
-        res.json(polls)
-    }catch(err){
-        next(err)
-    }
-})
+	try {
+		const polls = await Poll.find();
+		res.json(polls);
+	} catch (err) {
+		next(err);
+	}
+});
 
 export = router;
