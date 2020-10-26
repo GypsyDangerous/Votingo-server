d18a90b76652611f7f79af12ac669b064a176973
David Snyder <davidgraygs4@gmail.com>
2020-10-26 01:58:55 -0400

    create poll model

---
+++ src/models/poll.model.ts
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const pollSchema = new Schema(
+	{
+		name: { type: String, required: true },
+		description: { type: String, required: false },
+		options: { type: Object, required: true },
+		private: { type: Boolean, required: true },
+		creator: { type: Object, required: true },
+		uuid: { type: String, required: true },
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const poll = mongoose.model("poll", pollSchema);
+
+export = poll;
