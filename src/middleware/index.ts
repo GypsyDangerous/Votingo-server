acd3dc0d46a78a4e5ac56a27c70e693bf3c63e40
David Snyder <davidgraygs4@gmail.com>
2020-10-30 00:22:38 -0400

    updated types for some middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,11 +1,13 @@
-function notFound(req: any, res: any, next: any) {
+import { Request, Response, NextFunction } from "express";
+
+function notFound(req: Request, res: Response, next: NextFunction) {
 	res.status(404);
 	const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
 	next(error);
 }
 
 /* eslint-disable no-unused-vars */
-function errorHandler(err: any, req: any, res: any, next: any) {
+function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
 	/* eslint-enable no-unused-vars */
 	const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
 	res.status(statusCode);
